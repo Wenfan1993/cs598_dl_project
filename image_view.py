f4d33ec5d2fc7c5e5f3cbf1884fb4b3ce1ea01dd
Wenxi <wenfan1993@gmail.com>
2021-03-29 21:36:59 -0500

    update the directory

--- image_view.py
+++ image_view.py
@@ -8,18 +8,21 @@ Created on Mon Mar 29 20:48:41 2021
 from skimage.metrics import peak_signal_noise_ratio
 from skimage import img_as_float
 from scipy import ndimage as nd
-
+from skimage import io
 from matplotlib import pyplot as plt
-import scipy.io as io
+import scipy.io as scio
 import os
 
 #change the variable path_to_image to your local path
-path_to_image = '.\data'
+path_to_image = '..\data'
 os.chdir(path_to_image)
 
-mat_contents_mask = io.loadmat('training_data_seg1.mat')
-
+mat_contents_mask = scio.loadmat('training_data_seg1.mat')
 s_xt_label = mat_contents_mask['s_xt_label']
+image_obj = s_xt_label[0,:,:]
+image_obj.shape
+plt.imshow(image_obj)
 
-plt.imshow(s_xt_label[0,:,:])
-
+#guassian_img = nd.gaussian_filter(image_obj, sigma = 5)
+#guassian_img.shape
+#plt.imshow(guassian_img)
